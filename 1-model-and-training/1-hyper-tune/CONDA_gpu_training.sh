96b9ffa1d375aa2496a2e527cc6017b62f8e7e7c
Haoyu Yang <yanghaoyu97@outlook.com>
2023-02-08 11:19:28 +1000

    update GPU training script

--- 1-model-and-training/1-hyper-tune/CONDA_gpu_training.sh
+++ 1-model-and-training/1-hyper-tune/CONDA_gpu_training.sh
@@ -1,18 +1,13 @@
 #!/bin/bash 
-#PBS -N hypertune-GPU
-#PBS -l ncpus=48
-#PBS -l ngpus=4
-#PBS -l mem=168GB
+#PBS -N hypertune-GPU-conda
+#PBS -l ncpus=12
+#PBS -l ngpus=1
+#PBS -l mem=96GB
 #PBS -l jobfs=1GB
 #PBS -l walltime=1:00:00
 #PBS -q gpuvolta
 #PBS -P iu57
 #PBS -l storage=gdata/iu57+gdata/v88+gdata/dk92
-
-cd $PBS_O_WORKDIR
-
-# Request interactive GPU
-# qsub -I -q gpuvolta -lwd,walltime=1:00:00,ngpus=4,ncpus=48,mem=168GB,jobfs=1GB,storage=gdata/iu57+gdata/v88+gdata/dk92
 
 
 # Activate conda environment
@@ -20,11 +15,10 @@ eval "$(conda shell.bash hook)"  # https://hpc-unibe-ch.github.io/software/Anaco
 conda activate /g/data/v88/CONDA_ENV/tensorflow
 
 
-# Run Horovod
+# Run Keras tuner
+cd $PBS_O_WORKDIR
 python3 keras-tuner.py > tunerlog 2>&1
 
 
-# GPU monitoring
-# Ref: https://opus.nci.org.au/display/DAE/Gpustat+-+Multinode+GPU+monitoring
-# module load gpustat/1.0
-# gpustat-run <job_no>.gadi-pbs
+# Request interactive GPU
+# qsub -I -q gpuvolta -lwd,walltime=1:00:00,ngpus=4,ncpus=48,mem=168GB,jobfs=1GB,storage=gdata/iu57+gdata/v88+gdata/dk92
--- 1-model-and-training/1-hyper-tune/gpu_training.sh
+++ 1-model-and-training/1-hyper-tune/gpu_training.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
-#PBS -N hypertune-GPU-Gadi-env
-#PBS -l ncpus=48
-#PBS -l ngpus=4
-#PBS -l mem=168GB
+#PBS -N tuner-GPU-gadi-env
+#PBS -l ncpus=12
+#PBS -l ngpus=1
+#PBS -l mem=96GB
 #PBS -l jobfs=1GB
 #PBS -l walltime=1:00:00
 #PBS -q gpuvolta
@@ -10,22 +10,20 @@
 #PBS -l storage=gdata/iu57+gdata/v88+gdata/dk92
 #PBS -v PYTHONPATH=/g/data/v88/EXTRA_PYTHON_LIB/lib/python3.9/site-packages
 
-cd $PBS_O_WORKDIR
 
-# Request interactive GPU
-# qsub -I -q gpuvolta -lwd,walltime=1:00:00,ngpus=4,ncpus=48,mem=168GB,jobfs=1GB,storage=gdata/iu57+gdata/v88+gdata/dk92
-
-
-## Activate conda environment
+## Activate TensorFlow environment
 module use /g/data/dk92/apps/Modules/modulefiles
 module load NCI-ai-ml/22.11
 
 
-# Run Horovod
+# Run Keras tuner
+cd $PBS_O_WORKDIR
+export TF_GPU_THREAD_MODE="gpu_private"
 python3 keras-tuner.py > tunerlog 2>&1
 
 
+# Request interactive GPU
+# qsub -I -q gpuvolta -lwd,walltime=1:00:00,ngpus=4,ncpus=48,mem=168GB,jobfs=1GB,storage=gdata/iu57+gdata/v88+gdata/dk92
+
 # GPU monitoring
 # Ref: https://opus.nci.org.au/display/DAE/Gpustat+-+Multinode+GPU+monitoring
-# module load gpustat/1.0
-# gpustat-run <job_no>.gadi-pbs
