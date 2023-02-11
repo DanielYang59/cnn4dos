7cdc8719fd996e4d50b5848726b399e988ddac0b
Haoyu Yang <yanghaoyu97@outlook.com>
2023-02-11 13:17:47 +1000

    add time stamp to tuner log

--- 1-model-and-training/1-hyper-tune/gpu_tuner.sh
+++ 1-model-and-training/1-hyper-tune/gpu_tuner.sh
@@ -3,7 +3,7 @@
 #PBS -l ncpus=12
 #PBS -l ngpus=1
 #PBS -l mem=96GB
-#PBS -l jobfs=50GB
+#PBS -l jobfs=1GB
 #PBS -l walltime=24:00:00
 #PBS -q gpuvolta
 #PBS -P iu57
@@ -18,12 +18,9 @@ module load NCI-ai-ml/22.11
 
 # Run Keras tuner
 cd $PBS_O_WORKDIR
-python3 keras-tuner.py > tunerlog 2>&1
+python3 keras-tuner.py > "$(date +"%Y_%m_%d_%H").log" 2>&1
 
 
 # Request interactive GPU
 # qsub -I -q gpuvolta -lwd,walltime=1:00:00,ngpus=4,ncpus=48,mem=168GB,jobfs=1GB,storage=gdata/iu57+gdata/v88+gdata/dk92
-
-# GPU monitoring
-# Ref: https://opus.nci.org.au/display/DAE/Gpustat+-+Multinode+GPU+monitoring
 
