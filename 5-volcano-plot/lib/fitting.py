a744a335522ccd7d0a8beedc7e0c3602abfb085f
Haoyu <yanghaoyu97@outlook.com>
2023-02-16 14:00:25 +1000

    add fitting with automatic mixing

--- 5-volcano-plot/lib/fitting.py
+++ 5-volcano-plot/lib/fitting.py
@@ -1,5 +1,6 @@
 
 
+import sys
 import numpy as np
 from scipy import stats
 import pandas as pd
@@ -24,6 +25,7 @@ def linear_fitting_without_mixing(adsorption_energies, group_x, group_y, descrip
     assert isinstance(adsorption_energies, dict)
     assert isinstance(group_x, list)
     assert isinstance(group_y, list)
+    assert descriptor_x != descriptor_y
     assert isinstance(descriptor_x, str) and descriptor_x in group_x
     assert isinstance(descriptor_y, str) and descriptor_y in group_y
     assert not list(set(group_x).intersection(group_y))
@@ -71,14 +73,63 @@ def linear_fitting_without_mixing(adsorption_energies, group_x, group_y, descrip
     
 
 def linear_fitting_with_mixing(adsorption_energies, descriptor_x, descriptor_y, verbose=True):
+    # Check args
+    assert isinstance(adsorption_energies, dict)
+    assert descriptor_x != descriptor_y
+    assert isinstance(descriptor_x, str)
+    assert isinstance(descriptor_y, str)
     
     
+    # Add substrate name to row index (prep for stacking)
+    for key in adsorption_energies:
+        adsorption_energies[key] = adsorption_energies[key].rename(index=lambda x: f'{key}_{str(x)}')
+            
+    # Stack adsorption energy DataFrames
+    stacked_adsorption_energy_df = pd.concat(adsorption_energies.values())
     
-    pass
+    
+    # Loop through adsorbates
+    fitting_results = {}
+    for ads in list(stacked_adsorption_energy_df.columns.values):
+        if ads not in [descriptor_x, descriptor_y]:
+            
+            # Test mixing percentage
+            percentage_dict = {}
+            for percentage in range(101):
+                # Compile x array
+                x_array = np.array(stacked_adsorption_energy_df[descriptor_x]) * percentage * 0.01 + \
+                    np.array(stacked_adsorption_energy_df[descriptor_y]) * (100 - percentage) * 0.01
+                
+                # Perform linear fitting
+                result = stats.linregress(
+                    x_array, 
+                    np.array(stacked_adsorption_energy_df[ads]))
+                
+                percentage_dict[percentage] = result
+                
+                if verbose:
+                    print(f"Adsorbate: {ads}, {descriptor_x} {percentage} %, {descriptor_y} {100 - percentage} %, r2: {round(result.rvalue, 6)}.")
+                    
+            
+            # Get best and worst percentage
+            r2values = [i.rvalue for i in percentage_dict.values()]
+            best_r2 = max(r2values)
+            best_percentage = r2values.index(best_r2)
+            
+            print(f"Best R2 for {ads} is {round(best_r2, 4)}, {descriptor_x} {best_percentage} %. Worst R2 for {ads} is {round(min(r2values), 4)}, {descriptor_x} {r2values.index(min(r2values))} %.")
+          
+
+            # Perform fitting with best descriptor percentage
+            x_array = np.array(stacked_adsorption_energy_df[descriptor_x]) * best_percentage * 0.01 + \
+                    np.array(stacked_adsorption_energy_df[descriptor_y]) * (100 - best_percentage) * 0.01
+                
+            fitting_results[ads] = stats.linregress(
+                x_array, 
+                np.array(stacked_adsorption_energy_df[ads]))
     
     
-    
-    
+    return fitting_results
+   
     
 # Test area
 if __name__ == "__main__":
@@ -96,5 +147,6 @@ if __name__ == "__main__":
     group_y = ["4-OCH", "5-OCH2", "6-OCH3", "7-O", "8-OH", "11-HER"]
     descriptor_y = "8-OH"
     
-    linear_fitting_without_mixing(adsorption_energies, group_x, group_y, descriptor_x, descriptor_y)
+    # linear_fitting_without_mixing(adsorption_energies, group_x, group_y, descriptor_x, descriptor_y)
+    linear_fitting_with_mixing(adsorption_energies, descriptor_x, descriptor_y, verbose=False)
     
