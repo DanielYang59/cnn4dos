7c291c1d73557307fb652cb26a20112ab9155fad
Haoyu <yanghaoyu97@outlook.com>
2023-02-16 11:28:07 +1000

    add adsorption energy loader

---
+++ 5-volcano-plot/lib/data_loader.py
@@ -0,0 +1,25 @@
+
+
+import os
+import pandas as pd
+
+
+def load_adsorption_energy(path, substrates):
+    # Check args
+    assert os.path.isdir(path)
+    assert isinstance(substrates, list)
+
+    # Load adsorption energy by substrate
+    return {
+        sub: pd.read_csv(os.path.join(path, f"{sub}.csv"))
+        for sub in substrates
+    }
+    
+
+# Test area
+if __name__ == "__main__":
+    path = "../../0-dataset/label_adsorption_energy"
+    substrates = ["g-C3N4_is", "nitrogen-graphene_is", "vacant-graphene_is", "C2N_is", "BN_is", "BP_is"]
+    adsorbates = ["1-CO2", "2-COOH", "3-CO", "4-OCH", "5-OCH2", "6-OCH3", "7-O", "8-OH", "11-HER"]
+    
+    load_adsorption_energy(path, substrates, adsorbates)
