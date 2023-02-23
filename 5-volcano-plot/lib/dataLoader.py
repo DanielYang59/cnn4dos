815c4482a3f48aa9dc6091e714d261b1b94cc706
Haoyu <yanghaoyu97@outlook.com>
2023-02-23 14:39:03 +1000

    rename data loader

---
+++ 5-volcano-plot/lib/dataLoader.py
@@ -0,0 +1,63 @@
+
+
+import os
+import pandas as pd
+
+
+class dataLoader:
+    def __init__(self, debug=False, debug_dir="debug") -> None:
+        """Data loader class for volcano plots.
+
+        Args:
+            debug (bool, optional): debug mode, save all intermediate data to "debug" dir. Defaults to False.
+            debug_dir (str, optional): debug data saving dir. Defaults to "debug". 
+            
+        """
+        # Update attrib
+        self.debug = debug
+        self.debug_dir = debug_dir
+        
+    
+    def load_adsorption_energy(self, path, substrates, adsorbates):
+        """Load adsorption energy from csv file (without ZPE corrections).
+
+        Args:
+            path (path): path of adsorption energy storage directory.
+            substrates (list): list of substrates to be loaded
+            adsorbates (list): list of adsorbates to be loaded
+
+        Returns:
+            dict: dict of adsorption energies in pd.DataFrame, key is substrate name
+            
+        """
+        # Check args
+        assert os.path.isdir(path)
+        assert isinstance(substrates, list)
+        assert isinstance(adsorbates, list)
+
+        # Load adsorption energy by substrate
+        self.adsorption_energy_dict = {
+            sub: pd.read_csv(os.path.join(path, f"{sub}.csv"), index_col=0).loc[:, adsorbates]  # apply adsorbate filter
+            for sub in substrates
+        }
+    
+    
+    
+            
+
+
+        
+
+# Test area
+if __name__ == "__main__":
+    path = "../../0-dataset/label_adsorption_energy"
+    substrates = ["g-C3N4_is", "nitrogen-graphene_is", "vacant-graphene_is", "C2N_is", "BN_is", "BP_is"]
+    adsorbates = ["2-COOH", "3-CO", "4-OCH", "5-OCH2", "6-OCH3", "7-O", "8-OH", "11-H"]
+    
+    # Test adsorption energy loading
+    loader = dataLoader(debug=True)
+    loader.load_adsorption_energy(path, substrates, adsorbates)
+    # print(loader.adsorption_energy_dict)
+    
+    # Test add ZPE corrections
+    loader.add_thermal_correction(correction_file="../data/corrections_thermal.csv")
