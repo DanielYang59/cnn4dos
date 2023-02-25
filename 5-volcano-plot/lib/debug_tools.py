ad9b30457f1176a6c2fe31ee17eb29baa496701a
Haoyu Yang <yanghaoyu97@outlook.com>
2023-02-25 20:42:50 +1000

    add adsorption free energy output

--- 5-volcano-plot/lib/debug_tools.py
+++ 5-volcano-plot/lib/debug_tools.py
@@ -7,46 +7,78 @@ from utils import stack_adsorption_energy_dict
 
 
 class volcanoDebugger:
-    def __init__(self) -> None:
-        pass
-    
-    
-    def compare_limiting_potential(self):
-        pass
-    
-    
-    def calculate_adsorption_free_energy(self, output_dir="debug"):
+    def __init__(self, substrates, adsorbates, adsorption_energy_file, thermal_correction_file, adsorbate_free_energy_file, reaction_pathway_file, debug_dir="debug") -> None:
+        # Update attrib
+        self.substrates = substrates
+        self.adsorbates = adsorbates
+        
+        os.makedirs(debug_dir, exist_ok=True)
+        self.debug_dir = debug_dir
+        
+        
         # Initialize data loader
         loader = dataLoader()
         
-        
         # Load DFT adsorption energy
-        loader.load_adsorption_energy(
-            path="../../0-dataset/label_adsorption_energy",
-            substrates = ["g-C3N4_is", "nitrogen-graphene_is", "vacant-graphene_is", "C2N_is", "BN_is", "BP_is"],
-            adsorbates = ["2-COOH", "3-CO", "4-OCH", "5-OCH2", "6-OCH3", "7-O", "8-OH", "11-H"],
-        )
-                
-                
-        # Add thermal correction
-        loader.calculate_adsorption_free_energy(
-            correction_file="../data/corrections_thermal.csv",
-        )
+        loader.load_adsorption_energy(path=adsorption_energy_file, substrates=substrates, adsorbates=adsorbates)
+        
+        # Calculate adsorption free energy
+        loader.calculate_adsorption_free_energy(correction_file=thermal_correction_file)
+
+        self.adsorption_energy = loader.adsorption_energy
+        self.adsorption_free_energy = loader.adsorption_free_energy
+        
+        # Load free adsorbate energy
+        self.adsorbate_free_energy = loader.load_adsorbate_free_energy(path=adsorbate_free_energy_file)
+        
+        # Load reaction pathways
+        self.reaction_pathways = loader.load_reaction_pathway(file=reaction_pathway_file)        
+        
+
+    def calculate_adsorption_free_energy_MAE(self):
+        # Calculate adsorption free energy from DFT adsorption energy
         
         
-        # Output results
-        adsorption_free_energy = loader.adsorption_free_energy
-        print(f"Adsorption free energy data saved to directory \"{output_dir}\".")
-        os.makedirs(output_dir, exist_ok=True)
-        for sub, df in adsorption_free_energy.items():
-            df.to_csv(os.path.join(output_dir, f"adsorption_free_energy_{sub}.csv"))
+        # Calculate linear scaling relations of adsorption free energy
         
+        # Calculate mean absolute error (MAE)
+        
+        pass
+    
+        
+        
+    def calculate_limiting_potential_MAE(self,):
+        pass
+    
+    
+    def output_adsorption_free_energy(self):
+        """Output adsorption free energies.
+        
+        Notes:
+            1. The adsorption free energies were calculated by applying thermal corrections to adsorbed adsorbates.
+        
+        """
+        # Output adsorption free energy to csf files
+        for sub, df in self.adsorption_free_energy.items():
+            df.to_csv(os.path.join(self.debug_dir, f"adsorption_free_energy_{sub}.csv"))
+    
     
 # Test area
 if __name__ == "__main__":
     # Initialize debugger
-    debugger = volcanoDebugger()
+    debugger = volcanoDebugger(
+        substrates = ["g-C3N4_is", "nitrogen-graphene_is", "vacant-graphene_is", "C2N_is", "BN_is", "BP_is"],
+        adsorbates = ["2-COOH", "3-CO", "4-OCH", "5-OCH2", "6-OCH3", "7-O", "8-OH", "11-H"],
+        
+        adsorption_energy_file="../../0-dataset/label_adsorption_energy",
+        thermal_correction_file="../data/corrections_thermal.csv",
+        adsorbate_free_energy_file="../data/energy_adsorbate.csv",
+        reaction_pathway_file="../data/reaction_pathway.json",
+        
+        debug_dir="../debug"
+        )
+    
     
     # Calculate adsorption free energy
-    debugger.calculate_adsorption_free_energy()
+    debugger.output_adsorption_free_energy()
     
