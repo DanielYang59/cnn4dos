4527152ac884e5f6bfc72d4117db1b14c8d275e6
Haoyu Yang <yanghaoyu97@outlook.com>
2023-02-27 11:05:49 +1000

    temp save

--- 5-volcano-plot/lib/debug_tools.py
+++ 5-volcano-plot/lib/debug_tools.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
 from dataLoader import dataLoader
 import numpy as np
 import os
@@ -8,11 +12,12 @@ from utils import stack_adsorption_energy_dict
 
 
 class volcanoDebugger:
-    def __init__(self, substrates, adsorbates, descriptors, adsorption_energy_file, thermal_correction_file, adsorbate_free_energy_file, reaction_pathway_file, debug_dir="debug") -> None:
+    def __init__(self, substrates, adsorbates, descriptors, external_potential, adsorption_energy_file, thermal_correction_file, adsorbate_free_energy_file, reaction_pathway_file, debug_dir="debug") -> None:
         # Update attrib
         self.substrates = substrates
         self.adsorbates = adsorbates
         self.descriptors = descriptors
+        self.external_potential = external_potential
         
         os.makedirs(debug_dir, exist_ok=True)
         self.debug_dir = debug_dir
@@ -29,7 +34,7 @@ class volcanoDebugger:
 
         self.adsorption_energy = loader.adsorption_energy
         self.adsorption_free_energy = loader.adsorption_free_energy
-        
+
         # Load free adsorbate energy
         self.adsorbate_free_energy = loader.load_adsorbate_free_energy(path=adsorbate_free_energy_file)
         
@@ -109,7 +114,109 @@ class volcanoDebugger:
         
         overall_mae = sum(all_results.values()) / len(all_results)
         print(f"Overall MAE is {round(overall_mae, 4)} eV.")
+    
+    
+    def __calculate_limiting_potential(self, method, reaction_name):
+        
+        ########## Nested Function Starts ##########
+        def __calculate_free_energy_for_half_reaction(equation, stacked_adsorption_free_energy):
+            # Check args
+            assert isinstance(equation, dict)
+            assert isinstance(stacked_adsorption_free_energy, pd.DataFrame)
             
+            # Initialize free energy constant part
+            free_energy_constant_part = 0
+            
+            # Handle species in half reaction
+            for species, num in equation.items():
+                # Clean substrates ("*"): skip
+                if species == "*":
+                    continue
+                
+                
+                # Adsorbed species ("*CO2"): energy = G* + GadsCO2 + GCO2 
+                elif species.startswith("*"):
+                    species = species.lstrip("*")
+                   
+                               
+                    # Get free species energy
+                    free_species_energy = self.adsorbate_free_energy[species]
+                    free_energy_constant_part += (num * free_species_energy)
+                    
+                    # Get adsorption energy dataFrame
+                    #print(stacked_adsorption_free_energy)
+                    adsorption_energy_df = stacked_adsorption_free_energy[species]
+                    
+                    #print(adsorption_energy_df)
+                    
+                
+                
+                # Proton-electron pairs (PEP)
+                elif species == "PEP":
+                    pep_energy = 0.5 * self.adsorbate_free_energy["H2"] - self.external_potential
+                    free_energy_constant_part += (num * pep_energy)
+                    
+                    
+                # Non-adsorbed species ("CO2"): energy = GCO2
+                else:
+                    energy = self.adsorbate_free_energy[species.split("_")[0]]  # remove physical state suffix (_l, _g)
+                    
+                    free_energy_constant_part += (num * energy)
+                
+            
+        
+        
+        def calculate_limiting_potential_directly(reaction_pathway):
+            # Stack adsorption free energy
+            print(self.adsorption_free_energy)
+            stacked_adsorption_free_energy = stack_adsorption_energy_dict(self.adsorption_free_energy, add_prefix=False)
+            
+            
+            limiting_potential = {}
+            
+            # Calculate free energy change for each reaction step
+            for step_index, equation in reaction_pathway.items():
+                # Calculate products energy
+                # print(step_index, equation)
+                products_energy = __calculate_free_energy_for_half_reaction(equation["products"], stacked_adsorption_free_energy)
+                
+                
+                
+                # Calculate reactants energy
+                
+                
+                # Calculate free energy change
+                
+        
+        
+        def calculate_limiting_potential_with_scaling_relation(reaction_pathway):
+            pass
+        
+        
+        ########## Nested Function Ends ##########
+        
+        # Check args
+        assert method in {"direct", "scaling"}
+        
+        
+        # Load reaction pathway
+        assert reaction_name in self.reaction_pathways
+        reaction_pathway = self.reaction_pathways[reaction_name]
+        reaction_pathway.pop("comment")  # remove comment from reaction pathway dict
+        
+        
+        # Calculate limiting potential directly from energy
+        if method == "direct":
+            calculate_limiting_potential_directly(reaction_pathway)
+        
+        
+        # Calculate limiting potential directly from scaling relations
+        else:
+            calculate_limiting_potential_with_scaling_relation(reaction_pathway)
+        
+        
+        return #DEBUG
+    
     
     def calculate_adsorption_free_energy_MAE(self, mixing_percentages="AUTO"):
         """Calculate adsorption free energy MAE predicted by scaling relations.
@@ -136,9 +243,14 @@ class volcanoDebugger:
         adsorption_free_energy_MAE = self.__calculate_adsorption_energy_MAE(true_df=true_adsorption_free_energy, predicted_df=predicted_adsorption_free_energy, exclude_descriptors=True)
         
         
-    def calculate_limiting_potential_MAE(self,):
-        pass
-    
+    def calculate_limiting_potential_MAE(self, reaction):
+        # Calculate limiting potential directly from energy
+        self.__calculate_limiting_potential(method="direct", reaction_name=reaction)
+        
+        
+        # Calculate limiting potential using scaling relations
+        # self.__calculate_limiting_potential(method="scaling", reaction=reaction) #DEBUG
+        
     
     def output_adsorption_free_energy(self):
         """Output adsorption free energies.
@@ -156,16 +268,17 @@ class volcanoDebugger:
 if __name__ == "__main__":
     # Initialize debugger
     debugger = volcanoDebugger(
-        substrates = ["g-C3N4_is", "nitrogen-graphene_is", "vacant-graphene_is", "C2N_is", "BN_is", "BP_is"],
-        adsorbates = ["2-COOH", "3-CO", "4-OCH", "5-OCH2", "6-OCH3", "7-O", "8-OH", "11-H"],
+        substrates=["g-C3N4_is", "nitrogen-graphene_is", "vacant-graphene_is", "C2N_is", "BN_is", "BP_is"],
+        adsorbates=["2-COOH", "3-CO", "4-OCH", "5-OCH2", "6-OCH3", "7-O", "8-OH", "11-H"],
         descriptors=("3-CO", "8-OH"),
+        external_potential=0.17, 
         
         adsorption_energy_file="../../0-dataset/label_adsorption_energy",
         thermal_correction_file="../data/corrections_thermal.csv",
         adsorbate_free_energy_file="../data/energy_adsorbate.csv",
         reaction_pathway_file="../data/reaction_pathway.json",
         
-        debug_dir="../debug"
+        debug_dir="../debug",
         )
     
     
@@ -175,4 +288,8 @@ if __name__ == "__main__":
     
     # Calculate adsorption free energy MAE (with scaling relations)
     debugger.calculate_adsorption_free_energy_MAE()
+    
+    
+    # Calculate limiting potential MAE
+    debugger.calculate_limiting_potential_MAE(reaction="CO2RR_CH4")
     
