953140e1d50d4b824e664801ad7986ba626d6147
Haoyu Yang <yanghaoyu97@outlook.com>
2023-03-14 11:21:18 +1000

    add periodic table plotter

---
+++ z-supporting-info/activity-periodic-table/main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import yaml
+from src.load_limiting_potential import load_limiting_potential
+from src.plot_periodic_table import plot_periodic_table
+
+
+def plot_activity_periodic_table():
+     # Load config
+    with open("config.yaml") as ymlfile:
+        cfg = yaml.safe_load(ymlfile)
+    limiting_potential_file = cfg["path"]["limiting_potential_file"]
+    data_cols = cfg["path"]["data_cols"]
+    substrate = cfg["species"]["substrate"]
+    
+    
+    # Load limiting potential for selected substrate
+    limiting_potential = load_limiting_potential(limiting_potential_file, substrate=substrate, 
+                            name_col=data_cols["name"], potential_col=data_cols["limiting_potential"])
+    
+    
+    # Pass limiting potential to plotter
+    plot_periodic_table(
+    limiting_potential_dict=limiting_potential,
+    extended=False,  # show Lu/Lr elements
+    cmap="coolwarm_r",  # "coolwarm_r" for reversed Red-Blue colormap
+    cbar_height=390,  # colorbar height (should reposition to top right)
+    alpha=0.80,  # transparency
+    cbar_location=(0, 162)  #
+    )
+
+
+if __name__ == "__main__":
+   plot_activity_periodic_table()
