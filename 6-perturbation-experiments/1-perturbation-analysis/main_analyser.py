510d0b86b24195177ac75d97bacfcd7290060955
Haoyu Yang <yanghaoyu97@outlook.com>
2023-04-12 09:51:03 +1000

    add save interpolated DOS arg

--- 6-perturbation-experiments/1-perturbation-analysis/main_analyser.py
+++ 6-perturbation-experiments/1-perturbation-analysis/main_analyser.py
@@ -3,36 +3,46 @@
 
 
 dos_energy_range = (-14, 6, 4000)
+plot_energy_range = (-5, 5)
 project_data_dir = "data"
+project_results_dir = "results"
 extract_dos_from_vasprun_script = "../../0-utils/extract_dos_from_vasprunxml.py"
 extract_single_atom_dos_script = "../../0-utils/extract_single_atom_DOS.py"
 
 
 from pathlib import Path
-from lib.list_projects import list_projects
 from lib.analyze_dos_perturb import Analyzer
+from lib.list_projects import list_projects
+from lib.plot_dos_change import plot_dos_change
 
 
 # Main
 if __name__ == "__main__":
     # Let user choose project to work on
     project = list_projects(Path(project_data_dir))
-    
-    
+
+
     # Work on selected project
     atom_index = int(input("Extract DOS of which atom? (starts from 1)"))
     analyzer = Analyzer(rootpath=Path(project_data_dir) / project,
-                        atom_index=atom_index,
+                        atom_index=atom_index
                         )
-    
-    
+
+
     # Extract DOS of selected atom
     analyzer.extract_dos(Path(extract_dos_from_vasprun_script).resolve(),
                          Path(extract_single_atom_dos_script).resolve(),
-                         verbose=True,
+                         verbose=True
                          )
-    
-    
+
+
     # Calculate DOS change
-    analyzer.calculate_dos_change(energy_range=dos_energy_range)
-    
+    analyzer.calculate_dos_change(energy_range=dos_energy_range,
+                                  save_interpolated=True
+                                  )
+
+
+    # Plot DOS change at selected energy range
+    plot_dos_change(project_dir=Path(project_results_dir) / project,
+                    x_range=plot_energy_range
+                    )
