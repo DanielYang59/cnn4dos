2917295d48c9efc01143ef5c99e47dc2d1b1920b
Haoyu Yang <yanghaoyu97@outlook.com>
2023-04-11 19:34:26 +1000

    add DOS difference interpolation

--- 6-perturbation-experiments/1-perturbation-analysis/lib/analyze_dos_perturb.py
+++ 6-perturbation-experiments/1-perturbation-analysis/lib/analyze_dos_perturb.py
@@ -5,16 +5,17 @@
 import numpy as np
 import os
 import re
+from scipy.interpolate import interp1d
 import subprocess
 
 
 class Analyzer:
-    def __init__(self, rootpath, atom_index) -> None:
+    def __init__(self, rootpath, atom_index):
         """DOS analyzer for DOS perturbation experiments.
 
         Args:
             rootpath (Path): DOS data folder.
-            atom_index (int): index of atom to extract DOS from (starts from one)
+            atom_index (int): index of atom to extract DOS from (starts from one).
             
         """
         # Check working directory
@@ -31,14 +32,80 @@ class Analyzer:
         self.atom_index = atom_index
     
     
+    def __calculate_dos_change(self, original_dos_array, original_energy_array, perturbed_dos_array, perturbed_energy_array):
+        """Calculate DOS change.
+
+        Args:
+            original_dos_array (np.ndarray): _description_
+            original_energy_array (np.ndarray): _description_
+            perturbed_dos_array (np.ndarray): _description_
+            perturbed_energy_array (np.ndarray): _description_
+            
+        Returns:
+            shared_X (np.ndarray): share energy array
+            difference_array (np.ndarray): DOS difference array
+            
+        """
+        
+        
+        def interpolate_DOS(X, Ys, target_X):
+            """Interpolate DOS for all orbital channels.
+
+            Args:
+                X (np.ndarray): energy array
+                Ys (np.ndarray): DOS array in shape (NEDOS, numOrbitals)
+                target_X (np.ndarray): target energy array for DOS
+
+            Returns:
+                np.ndarray: interpolated DOS array in shape (numSamples, numOrbitals)
+                
+            """
+            # Check X and Y arrays shape
+            assert X.ndim == 1 and Ys.ndim == 2
+            assert target_X.ndim == 1
+            assert X.shape[0] == Ys.shape[0]
+            
+
+            # Interpolate each DOS orbital channel
+            resulted_DOSs = []
+            for channel_index in range(Ys.shape[1]):            
+                f1 = interp1d(X, Ys[:, channel_index])
+                resulted_DOSs.append(f1(target_X))
+                
+            return np.swapaxes(np.stack(resulted_DOSs), 0, 1)
+        
+        
+        # Check DOS and energy arrays
+        assert original_dos_array.shape[0] == original_energy_array.shape[0] == perturbed_dos_array.shape[0] == perturbed_energy_array.shape[0]
+        assert original_dos_array.shape[1] == perturbed_dos_array.shape[1]
+        
+        
+        # Create shared X array
+        x_min = max(original_energy_array[0], perturbed_energy_array[0])
+        x_max = min(original_energy_array[-1], perturbed_energy_array[-1])
+        step_length = (self.energy_range[1] - self.energy_range[0]) / self.energy_range[2]
+        shared_X = np.arange(x_min, x_max + step_length, step_length)
+        
+        
+        # Interpolate Y arrays
+        interpolated_original_array = interpolate_DOS(X=original_energy_array, Ys=original_dos_array, target_X=shared_X)
+        interpolated_perturbed_array = interpolate_DOS(X=perturbed_energy_array, Ys=perturbed_dos_array, target_X=shared_X)
+        
+        
+        # Calculate DOS difference
+        difference_array = interpolated_perturbed_array - interpolated_original_array
+        
+        return shared_X, difference_array
+        
+        
     def __extract_dos_for_dir(self, path, dos_script, single_dos_script):
         """DOS extraction worker for a folder.
 
         Args:
-            path (Path): DOS dir to extract single atom DOS
-            atom_index (int): index of atom to extract DOS
-            dos_script (Path): extract DOS from vasprun.xml script
-            single_dos_script (Path): extract single atom DOS script
+            path (Path): DOS dir to extract single atom DOS.
+            atom_index (int): index of atom to extract DOS.
+            dos_script (Path): extract DOS from vasprun.xml script.
+            single_dos_script (Path): extract single atom DOS script.
             
         """
         # Check path
@@ -96,33 +163,45 @@ class Analyzer:
         energy_array -= fermi_level
         
         
+        assert dos_array.shape[0] == energy_array.shape[0]
         return dos_array, energy_array        
-        
-        
+    
+    
     def calculate_dos_change(self, energy_range):
         # Check energy_range (roughly)
         assert len(energy_range) == 3 and isinstance(energy_range, tuple)
         self.energy_range = energy_range
         
         
-        # Get original DOS
-        self.__get_dos(self.original_path)
+        # Get original DOS and energy arrays
+        original_dos_array, original_energy_array = self.__get_dos(self.original_path)
         
         
         # For each folder under "path", calculate DOS change
-        for d in self.perturbed_path.iterdir():
-            if d.is_dir() and not d.name.startswith("."):
-                # print(d) #DEBUG
-                pass
+        for folder in self.perturbed_path.iterdir():
+            if folder.is_dir() and not folder.name.startswith("."):
+                # Get perturbed DOS and energy arrays
+                perturbed_dos_array, perturbed_energy_array = self.__get_dos(folder)
+                
+                
+                # Calculate DOS change
+                dos_change = self.__calculate_dos_change(
+                    original_dos_array, original_energy_array,
+                    perturbed_dos_array, perturbed_energy_array,
+                    )
+                
+                
+                import sys  #DEBUG
+                sys.exit()
                 
 
     def extract_dos(self, dos_script, single_dos_script, verbose=True):
         """Extract DOS from vasprun.xml for perturbation analysis.
 
         Args:
-            dos_script (Path): extract DOS from vasprun.xml script
-            single_dos_script (Path): extract single atom DOS script
-            verbose (bool): verbose during extraction
+            dos_script (Path): extract DOS from vasprun.xml script.
+            single_dos_script (Path): extract single atom DOS script.
+            verbose (bool): verbose during extraction.
             
         """
         # Work on "0-original" dir
@@ -159,7 +238,7 @@ if __name__ == "__main__":
     # # Test DOS extraction
     # analyzer.extract_dos(
     #                      Path("../../../0-utils/extract_dos_from_vasprunxml.py").resolve(),
-    #                      Path("../../../0-utils/extract_single_atom_DOS.py").resolve(),
+    #                      Path("../../../0-utils/extract_single_atom_DOS.py").resolve()
     #                      )
     
     
