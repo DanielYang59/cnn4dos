fe692fd2b579d9fb7761f7f93bc82362312763f9
Haoyu Yang <yanghaoyu97@outlook.com>
2023-04-12 20:52:47 +1000

    use pathlib

--- 2-model-evaluation/2-plot_parity_for_EACH.py
+++ 2-model-evaluation/2-plot_parity_for_EACH.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+from pathlib import Path
 import seaborn as sns
 import statsmodels.api as sm
 import sys
@@ -25,8 +26,8 @@ if __name__ == "__main__":
     with open("config.yaml") as ymlfile:
         cfg = yaml.safe_load(ymlfile)
     ## paths
-    feature_dir = cfg["path"]["feature_dir"]
-    label_dir = cfg["path"]["label_dir"]
+    feature_dir = Path(cfg["path"]["feature_dir"])
+    label_dir = Path(cfg["path"]["label_dir"])
     ## species
     substrates = cfg["species"]["substrates"]
     adsorbates = cfg["species"]["adsorbates"]
@@ -40,7 +41,7 @@ if __name__ == "__main__":
     remove_ghost = cfg["model_training"]["remove_ghost"]
 
     # Import model
-    model = tf.keras.models.load_model(os.path.join(model_dir, "model"))
+    model = tf.keras.models.load_model(Path(model_dir) / "model")
 
     # Import dataset from training.py at model directory
     ## Load dataset
