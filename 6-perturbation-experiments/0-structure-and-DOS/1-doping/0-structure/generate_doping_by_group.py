6e115bbeb97789311f9d1a689bd7fffb53225820
Haoyu Yang <yanghaoyu97@outlook.com>
2023-04-12 10:41:32 +1000

    remove execuatable priviledge

--- 6-perturbation-experiments/0-structure-and-DOS/1-doping/0-structure/generate_doping_by_group.py
+++ 6-perturbation-experiments/0-structure-and-DOS/1-doping/0-structure/generate_doping_by_group.py
@@ -2,7 +2,7 @@
 # Doping configs
 atoms_to_replace = "GROUP"  # index starts from 1 ("GROUP" for automatic grouping)
 new_element = "N"
-source_dir = "0-原始模型-Cr-vac-graphene"
+source_dir = "0-Cr-vac-graphene"
 output_dir = "grouped_nitrogen-doped"
 
 
@@ -21,7 +21,7 @@ if __name__ == "__main__":
             # Parse grouping results
             for line in grouping[1].split("\n")[1:]:
                 atoms_to_replace.append(int(line.split(":")[1]))
-    
+
     # Import source POSCAR
     assert os.path.exists(os.path.join(source_dir, "POSCAR"))
     source_poscar = POSCAR()
@@ -32,12 +32,12 @@ if __name__ == "__main__":
     for i in atoms_to_replace:
         # Check atom index
         assert isinstance(i, int) and i in range(1, sum(source_poscar.ion_numbers) + 1)
-        
+
         doped_poscar = copy.copy(source_poscar)
-        
+
         # Replace atom
         doped_poscar.replace_atom([i], new_element)
-        
+
         # Write new POSCAR
         os.makedirs(os.path.join(output_dir, str(i)), exist_ok=True)
         doped_poscar.write(filename=os.path.join(output_dir, str(i), "POSCAR"))
