e62c86fafd24167379996f065af0a69a60758c03
Haoyu Yang <yanghaoyu97@outlook.com>
2023-09-05 18:34:27 +0800

    move error raise into func

--- 4-shifting-experiments/main.py
+++ 4-shifting-experiments/main.py
@@ -6,8 +6,9 @@ from pathlib import Path
 
 from src.get_folders_in_dir import get_folders_in_dir
 from src.plot_shifting import plot_shifting
-from src.load_config import load_config
 from src.load_adsorbate import load_and_transpose_dos
+from src.load_config import load_config
+from src.load_cnn_model import load_cnn_model
 
 
 def main(configfile="config.yaml"):
@@ -20,16 +21,21 @@ def main(configfile="config.yaml"):
         directory_path=config["path"]["working_dir"],
         filter_file=config["shifting"]["dos_array_name"],
         )
-    if not folders:
-        raise FileNotFoundError(f"No suitable folders containing {config['shifting']['dos_array_name']} found in ({config['path']['working_dir']}).")
 
 
     # Load and transpose adsorbate array
     adsorbate_dos_array = load_and_transpose_dos(Path(config["path"]["adsorbate_dos_array_path"]))
 
 
+    # Load CNN model
+    cnn_model = load_cnn_model(Path(config["path"]["cnn_model_path"]))
+    cnn_model.summary()
 
-    # Plot shifting experiment as heatmap
+
+    # G
+
+
+    # # Plot shifting experiment as heatmap
     # plot_shifting(folders)
 
 
--- 4-shifting-experiments/src/get_folders_in_dir.py
+++ 4-shifting-experiments/src/get_folders_in_dir.py
@@ -7,17 +7,20 @@ from pathlib import Path
 
 def get_folders_in_dir(directory_path: str, filter_file: str = None) -> list:
     """
-    Get and return a list of folder names in the specified directory.
+    Get and return a list of folder names in the specified directory that optionally contain a specific file.
 
-    Args:
-        directory_path (str): The path to the directory to look in.
-        filter_file (str, optional): The name of a file to filter folders by.
-                                     Only folders containing this file will be returned.
-                                     Defaults to None.
+    Parameters:
+        directory_path (str): The absolute or relative path to the directory in which to search for folders.
+        filter_file (str, optional): A filename to filter the folders by. If specified, only folders containing
+                                     this file will be returned. Default is None, meaning all folders will be returned.
 
     Returns:
-        list: A list of folder names that match the criteria.
+        list: A list of folder paths that match the criteria. Each folder path is a `Path` object.
+
+    Raises:
+        FileNotFoundError: If no suitable folders are found, especially when a filter_file is specified and not found.
     """
+
     folder_list = []
     dir_path = Path(directory_path)
 
@@ -36,5 +39,8 @@ def get_folders_in_dir(directory_path: str, filter_file: str = None) -> list:
                     folder_list.append(dir_path / item.name)
             else:
                 folder_list.append(dir_path / item.name)
+
+    if not folder_list:
+        raise FileNotFoundError(f"No suitable folders containing {filter_file} found in ({directory_path}).")
 
     return folder_list
