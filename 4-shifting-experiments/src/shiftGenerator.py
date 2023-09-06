b8eac3594757a74840c6864635a8ffdacc987007
Haoyu Yang <yanghaoyu97@outlook.com>
2023-09-06 10:24:58 +0800

    add refactored shiftGenerator

---
+++ 4-shifting-experiments/src/shiftGenerator.py
@@ -0,0 +1,81 @@
+#!/bin/usr/python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import warnings
+
+class ShiftGenerator:
+    def __init__(self, dos_array: np.ndarray, shifting_range: list, shift_step: float, shifting_orbitals: list):
+        """
+        Initialize the ShiftGenerator.
+
+        Args:
+            dos_array (np.ndarray): The pre-processed DOS array of shape (numSamplings, numOrbitals, 1).
+            shifting_range (list): A list containing the start and end of the shifting range.
+            shift_step (float): The step length for each shift.
+            shifting_orbitals (list): A list containing the indices of the orbitals to be shifted.
+
+        Raises:
+            ValueError: If the shape of dos_array is not as expected, or if shifting parameters are not valid.
+        """
+        # Check the shape of dos_array
+        numSamplings, numOrbitals, _ = dos_array.shape
+        if numSamplings <= 500:
+            warnings.warn("The number of samplings is not greater than 500.")
+        if numOrbitals not in {1, 4, 9, 16}:
+            raise ValueError("numOrbitals must be one of {1, 4, 9, 16}")
+
+        # Check shifting parameters
+        start, end = shifting_range
+        if start >= end:
+            raise ValueError("The start of the shifting range should be smaller than the end.")
+        if shift_step <= 0:
+            raise ValueError("shift_step should be greater than zero.")
+        if (end - start) % shift_step != 0:
+            raise ValueError("The shifting range (end - start) should be a multiple of shift_step.")
+        if len(set(shifting_orbitals)) != len(shifting_orbitals):
+            raise ValueError("Duplicate orbital indices found in shifting_orbitals.")
+        if any((idx < 0 or idx >= numOrbitals) for idx in shifting_orbitals):
+            raise ValueError(f"Invalid orbital indices in shifting_orbitals. Valid range is [0, {numOrbitals-1}]")
+
+        self.dos_array = dos_array
+        self.shifting_range = shifting_range
+        self.shift_step = shift_step
+        self.shifting_orbitals = shifting_orbitals
+
+    def generate_shifted_arrays(self):
+        """
+        Generate a series of shifted DOS arrays.
+
+        Returns:
+            list: A list of shifted DOS arrays.
+
+        """
+        start, end = self.shifting_range
+        shift_values = np.arange(start, end + self.shift_step, self.shift_step)
+
+        shifted_arrays = []
+
+        for shift_value in shift_values:
+            # Create a copy to store the shifted array
+            shifted_dos = np.copy(self.dos_array)
+
+            for orbital_idx in self.shifting_orbitals:
+                # Shift the data along the numSamplings axis
+                if shift_value > 0:
+                    shifted_dos[:, orbital_idx, 0] = np.pad(
+                        shifted_dos[int(shift_value):, orbital_idx, 0],
+                        (0, int(shift_value)),
+                        'constant',
+                        constant_values=0
+                    )
+                elif shift_value < 0:
+                    shifted_dos[:, orbital_idx, 0] = np.pad(
+                        shifted_dos[:int(shift_value), orbital_idx, 0],
+                        (-int(shift_value), 0),
+                        'constant',
+                        constant_values=0
+                    )
+            shifted_arrays.append(shifted_dos)
+
+        return shifted_arrays
