680326c73231c71449586fba0196636b631dc3df
Haoyu Yang <yanghaoyu97@outlook.com>
2023-09-06 17:32:16 +0800

    add missing dos calculation resolution variable

--- 4-shifting-experiments/main.py
+++ 4-shifting-experiments/main.py
@@ -9,25 +9,29 @@ from src.cnnPredictor import CNNPredictor
 from src.dataLoader import DataLoader
 from src.dosProcessor import DOSProcessor
 from src.shiftGenerator import ShiftGenerator
-from src.shiftPlotter import shiftPlotter
+from src.shiftPlotter import ShiftPlotter
 from src.utilities import get_folders_in_dir
 
 def main():
+    import sys  #DEBUG
     # Step 1: Load config and adsorbate DOS
     data_loader = DataLoader()
-    config = data_loader.load_config('path/to/config.yaml')
+    config = data_loader.load_config('config.yaml')
 
-    adsorbate_dos = data_loader.load_and_preprocess_adsorbate_dos(config['path']['adsorbate_dos_array_path'])
+    adsorbate_dos = data_loader.load_and_preprocess_adsorbate_dos(
+        config['path']['adsorbate_dos_array_path'],
+        config['shifting']['max_adsorbate_channels'],
+        )
 
     # Step 2: List all matched folders
     working_dir = config['path']['working_dir']
     folders = get_folders_in_dir(working_dir, filter_file=config['shifting']['dos_array_name'])
 
     # Load the CNN model
-    cnn_model = tf.keras.models.load_model(Path(config['path']['cnn_model_path']) / "model")
+    cnn_model = tf.keras.models.load_model(Path(config['path']['cnn_model_path']))
 
     # Create an instance of CNNPredictor
-    cnn_predictor = CNNPredictor(cnn_model)
+    cnn_predictor = CNNPredictor(loaded_model=cnn_model)
 
     # Create an empty list to store predictions for future plotting
     all_predictions = []
@@ -37,11 +41,18 @@ def main():
         dos_file_path = folder / config['shifting']['dos_array_name']
 
         # a. Load DOS array with DOSProcessor
-        dos_processor = DOSProcessor(dos_file_path)
+        unshifted_dos = data_loader.load_unshifted_dos(dos_file_path)
+        dos_processor = DOSProcessor(unshifted_dos)
         processed_dos = dos_processor.remove_ghost_state()
 
         # b. Generate shifting arrays with ShiftGenerator
-        shift_gen = ShiftGenerator(processed_dos, config['shifting'])
+        shift_gen = ShiftGenerator(
+            processed_dos,
+            dos_calculation_resolution=config['shifting']['dos_calculation_resolution'],
+            shifting_range=config['shifting']['shifting_range'],
+            shifting_step=config['shifting']['shifting_step'],
+            shifting_orbitals=config['shifting']['shifting_orbitals']
+            )
         shifted_dos_arrays = shift_gen.generate_shifted_arrays()
 
         # c. Feed each shifted array into the CNN model for prediction
--- 4-shifting-experiments/src/shiftGenerator.py
+++ 4-shifting-experiments/src/shiftGenerator.py
@@ -5,7 +5,7 @@ import numpy as np
 import warnings
 
 class ShiftGenerator:
-    def __init__(self, dos_array: np.ndarray, shifting_range: list, shifting_step: float, shifting_orbitals: list):
+    def __init__(self, dos_array: np.ndarray, shifting_range: list, shifting_step: float, shifting_orbitals: list, dos_calculation_resolution: float):
         """
         Initialize the ShiftGenerator.
 
@@ -14,16 +14,19 @@ class ShiftGenerator:
             shifting_range (list): A list containing the start and end of the shifting range.
             shifting_step (float): The step length for each shift.
             shifting_orbitals (list): A list containing the indices of the orbitals to be shifted.
+            dos_calculation_resolution (float): The energy resolution along the numSamplings axis of the DOS array.
 
         Raises:
             ValueError: If the shape of dos_array is not as expected, or if shifting parameters are not valid.
         """
         # Check the shape of dos_array
-        numSamplings, numOrbitals, _ = dos_array.shape
+        numSamplings, numOrbitals, numChannels = dos_array.shape
         if numSamplings <= 500:
             warnings.warn("The number of samplings is not greater than 500.")
         if numOrbitals not in {1, 4, 9, 16}:
             raise ValueError("numOrbitals must be one of {1, 4, 9, 16}")
+        if numChannels != 1:
+            raise ValueError("numChannels must be 1")
 
         # Check shifting parameters
         start, end = shifting_range
@@ -37,11 +40,14 @@ class ShiftGenerator:
             raise ValueError("Duplicate orbital indices found in shifting_orbitals.")
         if any((idx < 0 or idx >= numOrbitals) for idx in shifting_orbitals):
             raise ValueError(f"Invalid orbital indices in shifting_orbitals. Valid range is [0, {numOrbitals-1}]")
+        if not self._is_multiple_of(shifting_step, dos_calculation_resolution):
+            raise ValueError("shifting_step must be a multiple of dos_calculation_resolution.")
 
         self.dos_array = dos_array
         self.shifting_range = shifting_range
         self.shift_step = shifting_step
         self.shifting_orbitals = shifting_orbitals
+        self.dos_calculation_resolution = dos_calculation_resolution
 
     def _is_multiple_of(self, number, base):
         """
@@ -63,33 +69,44 @@ class ShiftGenerator:
 
         Returns:
             list: A list of shifted DOS arrays.
-
         """
         start, end = self.shifting_range
         shift_values = np.arange(start, end + self.shift_step, self.shift_step)
-
         shifted_arrays = []
 
         for shift_value in shift_values:
             # Create a copy to store the shifted array
             shifted_dos = np.copy(self.dos_array)
 
+            # Calculate the number of indices to shift based on dos_calculation_resolution
+            num_indices_to_shift = int(abs(shift_value) / self.dos_calculation_resolution)
+
+            if num_indices_to_shift >= shifted_dos.shape[0]:
+                print(f"Skipping shift value {shift_value} as it is greater or equal to the data shape.")
+                continue
+
             for orbital_idx in self.shifting_orbitals:
-                # Shift the data along the numSamplings axis
+                # First, perform the shifting
                 if shift_value > 0:
-                    shifted_dos[:, orbital_idx, 0] = np.pad(
-                        shifted_dos[int(shift_value):, orbital_idx, 0],
-                        (0, int(shift_value)),
-                        'constant',
-                        constant_values=0
-                    )
+                    shifted_data = shifted_dos[num_indices_to_shift:, orbital_idx, 0]
                 elif shift_value < 0:
-                    shifted_dos[:, orbital_idx, 0] = np.pad(
-                        shifted_dos[:int(shift_value), orbital_idx, 0],
-                        (-int(shift_value), 0),
-                        'constant',
-                        constant_values=0
-                    )
+                    shifted_data = shifted_dos[:-num_indices_to_shift, orbital_idx, 0]
+                else:  # shift_value == 0
+                    shifted_data = shifted_dos[:, orbital_idx, 0]
+
+                # Next, perform the zero-padding
+                padding = np.zeros(num_indices_to_shift)
+                if shift_value != 0:
+                    if shift_value > 0:
+                        padded_data = np.concatenate([padding, shifted_data])
+                    else:  # shift_value < 0
+                        padded_data = np.concatenate([shifted_data, padding])
+                else:  # shift_value == 0
+                    padded_data = shifted_data
+
+                # Assign the padded data back to the array
+                shifted_dos[:, orbital_idx, 0] = padded_data
+
             shifted_arrays.append(shifted_dos)
 
         return shifted_arrays
