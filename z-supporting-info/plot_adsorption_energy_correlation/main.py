f09cc2b5f944105e051c68733404891ebd5bda53
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-01-14 17:56:50 +0800

    revise corr map plotter

--- z-supporting-info/plot_adsorption_energy_correlation/main.py
+++ z-supporting-info/plot_adsorption_energy_correlation/main.py
@@ -16,7 +16,6 @@ if __name__ == "__main__":
     # Generate correlation matrix
     corr_matrix = generate_corr_matrix(config_file="config.yaml")
 
-
     # Reformat molecule names
     revised_names = [format_mol_name(name) for name in corr_matrix.index.values]
 
@@ -24,16 +23,15 @@ if __name__ == "__main__":
     corr_matrix.index = revised_names
     corr_matrix.columns = revised_names
 
-
     # Plot correlation map
     sns.set(color_codes=True, font_scale=1.2)
     plt.figure(figsize=(8, 7))
-    corrplot(corr_matrix,
-            size_scale=500, marker="s",  # shape of the marker
-             )
+    corrplot(
+        corr_matrix,
+        size_scale=500, marker="s",  # shape of the marker
+        )
 
     plt.savefig(os.path.join("figures", "Eads_correlation_map.png"), bbox_inches="tight", dpi=300)
-
 
     # Show correlation coefficients
     print(corr_matrix)
