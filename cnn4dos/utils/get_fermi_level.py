c997f68f57bd8077e5fef5b8da2c118303cc0a57
Haoyu Yang <yanghaoyu97@outlook.com>
2024-02-19 16:03:28 +0800

    add arg for digits select

--- cnn4dos/utils/get_fermi_level.py
+++ cnn4dos/utils/get_fermi_level.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from pymatgen.io.vasp import Outcar, Vasprun, Wavecar
 
 
-def get_fermi_level(path: Path) -> float:
+def get_fermi_level(path: Path, digits: int = 4) -> float:
     """Get the Fermi level from VASP output files.
 
     This function searches for the Fermi level in VASP output files
@@ -13,6 +13,8 @@ def get_fermi_level(path: Path) -> float:
 
     Parameters:
         path (Path): The directory path to the VASP task.
+        digits (Optional[int]): The number of digits to keep after
+            the decimal point. Defaults to 4.
 
     Returns:
         float: The Fermi level.
@@ -26,11 +28,13 @@ def get_fermi_level(path: Path) -> float:
     wavecar_file = path / "WAVECAR"
 
     if vasprun_file.is_file():
-        return Vasprun(vasprun_file).efermi
+        fermi_level = Vasprun(vasprun_file).efermi
     elif outcar_file.is_file():
-        return Outcar(outcar_file).efermi
+        fermi_level = Outcar(outcar_file).efermi
     elif wavecar_file.is_file():
-        return Wavecar(wavecar_file).efermi
+        fermi_level = Wavecar(wavecar_file).efermi
 
     else:
         raise FileNotFoundError("Cannot find any file containing fermi level.")
+
+    return round(fermi_level, digits)
